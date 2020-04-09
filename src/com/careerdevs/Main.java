25cd4d892af25fa3b84d610a403a94a9134f6395
jfdoyle3 <indellibleink@gmail.com>
2020-04-08 23:14:20 -0400

    update Main.java

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,18 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Intro
+       //PrimitiveTypes displayPrimitives=new PrimitiveTypes();
+       // displayPrimitives.primitives();
+
+       // ReferenceTypes displayReferences=new ReferenceTypes();
+      //  displayReferences.references();
+
+        Arrays displayArrays=new Arrays();
+        displayArrays.arrays();
+
+    }
+}
